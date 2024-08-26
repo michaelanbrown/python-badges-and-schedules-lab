65550e56b0e3bf74457afc4de7e7b680ebf74517
michaelanbrown <m_brownbrown@yahoo.com>
2024-08-26 12:09:54 -0400

    passed the third test

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -7,8 +7,12 @@ def batch_badge_creator(names):
 def assign_rooms(names):
     rooms = range(1,9)
 
+    assignments = []
     for room in rooms:
         assignments.append(f'Hello, {names[room - 1]}! You\'ll be assigned to room {room}!')
+
+    return assignments
+
 
 def printer(names):
     return None
